ad96dcbd3463a37c04f0a264117f72e5447f5df8
adsdeworst <advaithvecham@hotmail.com>
2024-02-12 21:43:27 -0800

    haskdhjshka

--- USACO_Problems/closing_the_farm/crf.py
+++ USACO_Problems/closing_the_farm/crf.py
@@ -7,6 +7,9 @@ TASK:
 import sys
 from collections import deque
 
+
+sys.setrecursionlimit(10 ** 6)
+
 # sys.stdin = open("closing.in", "r")
 # sys.stdout = open("closing.out", "w")
 
@@ -26,9 +29,10 @@ closed = [False] * N
 def dfs(node):
     q = deque(paths[node])
     for i in q:
-        if visited[i] == False or closed[i] == False:
+        if visited[i] == False and closed[i] == False:
             visited[i] = True
             dfs(i)
+        
         
 dfs(0)
 if visited.count(True) == N:
@@ -50,53 +54,3 @@ for i in range(N - 1):
         print("YES")
     else:
         print("NO")
-
-# import sys
-# from collections import deque
-
-# # sys.stdin = open("closing.in", "r")
-# # sys.stdout = open("closing.out", "w")
-
-# n, m = map(int, input().split())
-# roads = []
-# closing_order = []
-# for _ in range(m):
-#     roads.append(list(map(int, input().strip("\n").split())))
-# for _ in range(n):
-#     closing_order.append(int(input().strip("\n")) - 1)
-
-# def dfs(node, not_inc):
-#     q = deque(connected_barns[node])
-#     for i in q:
-#         if visited[i] and i in not_inc:
-#             continue
-        
-#         visited[i] = True
-#         dfs(i, not_inc)
-# for l in range(n):
-#     connected_barns = [[] for _ in range(n)]
-#     closed_barns = []
-#     rep_barns = []
-#     visited = [False] * n
-#     for k in closed_barns:
-#         visited[k] = True
-#     for i in range(m):
-#         a, b = roads[i]
-#         connected_barns[a - 1].append(b - 1)
-#         connected_barns[b - 1].append(a - 1)
-            
-#     for i in range(len(closing_order)): 
-#         if visited[i]:
-#             continue
-
-#         visited[i] = True
-#         rep_barns.append(i)
-#         dfs(i, closed_barns)
-        
-#     closed_barns.append(closing_order[0])
-#     closing_order.pop(0)   
-#     if len(rep_barns) > 2:
-#         print("NO")
-#     else:
-#         print("YES")
-    
