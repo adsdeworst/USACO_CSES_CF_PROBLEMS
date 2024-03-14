c3b953a170ccd33fc9abeb65fd2479870c08399e
Advaith Vecham <advaithvecham@hotmail.com>
2024-03-13 22:34:40 -0700

    cowpetency score

--- USACO_Problems/cowpetency_score/cs.py
+++ USACO_Problems/cowpetency_score/cs.py
@@ -1,11 +1,4 @@
-import sys
+t = int(input())
 
-def main():
-    continue
-
-def cowpetency(cows, intervals, max_c, cow_list, interval_list):
-    for i in interval_list:
-        a, h = i
-        start = max(cow_list[:a + 1])
-        mid = max(cow_list[a+1:h])
-        
+n, q, c = list(map(int, input().split()))
+      
